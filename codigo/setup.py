e80b0d3d26572331a7a69724b101ef7ec9f1f5be
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-08-05 19:30:05 -0300

    final da atualização 1.5

--- codigo/setup.py
+++ codigo/setup.py
@@ -8,16 +8,19 @@ build_exe_options = {
     "include_files":['img/']
     }
 
+icone = "img/icone_prin.ico"
+
 # GUI applications require a different base on Windows (the default is for
 # a console application).
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+      
 
 setup(
     name="V-Audio",
     version="1.5",
     description="Ativa atalhos para alterar o volume do sistema.",
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base=base)]
+    executables=[Executable("main.py", base=base ,icon="img/icone_prin.ico")]
 )
